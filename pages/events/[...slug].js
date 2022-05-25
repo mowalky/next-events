a28a588b5df07d218e8025d07dd8a139d2e86854
Mowalky <mowalky@outlook.com>
2022-05-24 21:16:07 -0500

    setting up pages

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilteredEvents() {
+  return (
+    <div>
+      <h1>The Events Filtered</h1>
+    </div>
+  );
+}
+
+export default FilteredEvents;
---
+++ pages/events/[eventid].js
@@ -0,0 +1,9 @@
+function EventDetail() {
+  return (
+    <div>
+      <h1>The Events Detail</h1>
+    </div>
+  );
+}
+
+export default EventDetail;
