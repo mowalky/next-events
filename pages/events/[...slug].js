8fa0860f8d322c997c20d4481548bbf0351f0e93
Mowalky <mowalky@outlook.com>
2022-05-27 10:30:00 -0500

    updated

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,46 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+
+import EventList from "../../components/events/event-list";
+
 function FilteredEvents() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found</p>;
+  }
+  console.log(filteredEvents);
+
   return (
     <div>
-      <h1>The Events Filtered</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 }
