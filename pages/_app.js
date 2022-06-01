995c8b27efeda2e5b7e219a618aaae60044ec44c
Mowalky <mowalky@outlook.com>
2022-06-01 07:51:08 -0500

    updated notification

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Notification title="test" message="this is a test" status="success" />
     </Layout>
   );
 }
