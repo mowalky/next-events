5b7cee0b0b275f1d5416b86c61b5f9ef2ed5a5ae
Mowalky <mowalky@outlook.com>
2022-06-01 07:56:33 -0500

    setup notification context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Notification title="test" message="this is a test" status="success" />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Notification title="test" message="this is a test" status="success" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
