1577e3308602ba7c69d767a1ce06aa566997dc99
Mowalky <mowalky@outlook.com>
2022-06-01 08:01:23 -0500

    notification context updates

--- store/notification-context.js
+++ store/notification-context.js
@@ -2,13 +2,29 @@ import { createContext, useState } from "react";
 
 const NotificationContext = createContext({
   notification: null,
-  showNotification: () => {},
+  showNotification: (notificationData) => {},
   hideNotification: () => {},
 });
 
 export function NotificationContextProvider(props) {
+  const [notification, setNotification] = useState(null);
+
+  function showNotificationHandler(notificationData) {
+    setNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setNotification(null);
+  }
+
+  const context = {
+    notification: notification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
